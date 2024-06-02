c32f0f747ea981110fadcc37a2c8a8eaede9964e
Sven Palberg <sven@palberg.de>
2024-06-02 17:19:52 +0200

    Update Deno dependencies and refactor poem handling

--- src/examples/complex/main.ts
+++ src/examples/complex/main.ts
@@ -1,6 +1,6 @@
 import { say } from "cowsay";
 import { bgBlack, yellow } from "@std/fmt/colors";
-import { dirname, normalize } from "@std/url";
+import poem from "./poem.json" with { type: 'json' };
 
 const msg = prompt("Please provide the message:");
 
@@ -9,6 +9,4 @@ console.log(say({
 }));
 
 console.log("\n\nHere is a poem for you:\n");
-const poem = await fetch(normalize(dirname(import.meta.url) + "/poem.txt"))
-  .then((res) => res.text());
-console.log(bgBlack(yellow(poem)));
+console.log(bgBlack(yellow(poem.verses.join("\n"))));
